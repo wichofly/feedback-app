8ef3fef6561e4d58c62c4a4cc0467d350dbdcd0c
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-15 16:03:37 +0200

    Animation

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,8 +1,9 @@
-import FeedbackItem from './FeedbackItem';
+import { motion, AnimatePresence } from 'framer-motion';
 // import PropTypes from 'react';
 
-// feedback is working as props, i need to write this prop in app.js
+import FeedbackItem from './FeedbackItem';
 
+// feedback is working as props, i need to write this prop in app.js
 function FeedbackList({ feedback, handleDelete }) {
   // we do have a feedback so it wont be displayed. the feedback come from "useState(FeedbackData)"
   if (!feedback || feedback.length === 0) {
@@ -11,11 +12,32 @@ function FeedbackList({ feedback, handleDelete }) {
 
   return (
     <div className="feedback-list">
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
+
+  // return (
+  //   <div className="feedback-list">
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
 }
 
 // FeedbackList.propTypes = {
