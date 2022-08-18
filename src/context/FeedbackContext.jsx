3ed065166c5a038e863dee2711ea263f5dd3c9af
Mauricio Erazo <antonierazo13@hotmail.com>
2022-08-18 09:58:04 +0200

    Proxy

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -21,7 +21,7 @@ export const FeedbackProvider = ({ children }) => {
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch('http://localhost:5000/feedback?_sort=id&_order=desc')
+    const response = await fetch('/feedback?_sort=id&_order=desc')
     const data = await response.json()
 
     setFeedback(data)
